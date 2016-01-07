dbe78e48bf8dc5b5a3550d73d8b6ffe7b3bad071
Chris Lin <ti.lin@kodakalaris.com>
2016-01-07 09:30:54 -0500

    Initial Commit

---
+++ GithubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubTest
+//
+//  Created by Chris Lin on 1/7/16.
+//  Copyright © 2016 Chris Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GithubTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GithubTest
+//
+//  Created by Chris Lin on 1/7/16.
+//  Copyright © 2016 Chris Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
